9d117fbfbe3b25de2b79a2eea4824d839d62c82f
Arthur Fabricyo <arthur.fabricyo@gmail.com>
2025-08-14 14:10:34 -0300

    first commit

---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,12 @@
+
+export function App(){
+  return (
+      <div className="creator">Powered by electron-vite
+      <h1 className="text-violet-400 ">
+        Hello world
+
+        E o vasco ?
+      </h1>
+      </div>
+  )
+}
